67169d6c00e4575616450fde13a71aecc44caee9
guptavikramdev <myselfvikramgupta@gmail.com>
2024-02-18 21:47:29 +0530

    fixed error middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,28 +1,25 @@
-import { DEBUG_MODE } from '../config'
-import {ValidationError} from 'joi'
-import CustomErrorHandler from '../services/CustomErrorHandler' 
- const errorHandler=(err,req,res,next) => {
-    let statusCode=500;
-    let data={
-        massage:"Internal Server Error",
-        ...(DEBUG_MODE=="true" && {originalMassage: err.message})
-    }
+import { DEBUG_MODE } from "../config";
+import { ValidationError } from "joi";
+import CustomErrorHandler from "../services/CustomErrorHandler";
+const errorHandler = (err, req, res, next) => {
+  let statusCode = 500;
+  let data = {
+    massage: "Internal Server Error",
+    ...(DEBUG_MODE == "true" && { error: err.message }),
+  };
 
-    if(err instanceof ValidationError) {
-        statusCode=401;
-        data={
-            massage:err.message,
-        }
-       
-    }
-    if(err instanceof CustomErrorHandler) {
-        statusCode=err.starus;
-        data={
-            massage:err.message,
-        }
-       
-    }
-    return res.status(statusCode).json(data)
-
-}
-export default errorHandler
+  if (err instanceof ValidationError) {
+    statusCode = 401;
+    data = {
+      massage: err.message,
+    };
+  }
+  if (err instanceof CustomErrorHandler) {
+    statusCode = err.starus;
+    data = {
+      massage: err.message,
+    };
+  }
+  return res.status(statusCode).json(data);
+};
+export default errorHandler;
